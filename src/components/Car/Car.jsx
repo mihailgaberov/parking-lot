3db72faae7db750e4336d118a425ed3b4f82b8d1
Mihail Gaberov <mihail.gaberov@gmail.com>
2022-05-02 14:34:27 +0200

    :dizzy: Building car animation

--- src/components/Car/Car.jsx
+++ src/components/Car/Car.jsx
@@ -2,26 +2,26 @@ import { useState } from "react";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 
 import carUrl from "../../../assets/car.png";
-import styles from "./car.module.scss";
-
+import "./car.css";
 
 export const Car = () => {
   const [state, setState] = useState(true);
+
   return (
-    <>
-      <SwitchTransition mode={"out-in"}>
+      <div className="car-container">
+        <SwitchTransition mode={"out-in"}>
         <CSSTransition
-          key={state}
-          addEndListener={(node, done) => {
-            node.addEventListener("transitionend", done, false);
-          }}
-          classNames={styles.fade}
-        >
-          <div className={styles.carContainer}>
-            <img src={carUrl} alt='Car' onClick={() => setState((state) => !state)} />
-          </div>
-        </CSSTransition>
-      </SwitchTransition>
-    </>
+              key={state}
+              addEndListener={(node, done) => {
+                node.addEventListener("transitionend", done, false);
+              }}
+              classNames="fade"
+          >
+            <div className="button-container">
+              <img className='car' src={carUrl} alt='Car' onClick={() => setState((state) => !state)} />
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
+      </div>
   );
 };
