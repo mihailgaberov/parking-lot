b239d45def88201b9c288a239d8cbcfb97dce0fa
mihail.gaberov <mihail.gaberov@ysura.com>
2022-05-03 09:26:13 +0200

    ✨ Add main logic class

---
+++ src/lib/parking-lot.js
@@ -0,0 +1,41 @@
+class ParkingLot {
+  slots = [];
+
+  constructor(parkingSize) {
+    this.slots = new Array(parkingSize).fill(null);
+  }
+
+  park(carId) {
+    if (this.slots.every((slot) => slot !== null)) {
+      return false;
+    }
+
+    for (let i = 0; i <= this.slots.length; i++) {
+      const slot = this.slots[i];
+
+      if (slot === null) {
+        this.slots[i] = carId;
+        return true;
+      }
+    }
+  }
+
+  getSlots() {
+    return this.slots;
+  }
+
+  remove(carId) {
+    if (this.slots.every((slot) => slot !== carId)) {
+      return false;
+    }
+
+    for (let i = 0; i <= this.slots.length; i++) {
+      const slot = this.slots[i];
+
+      if (slot === carId) {
+        this.slots[i] = null;
+        return true;
+      }
+    }
+  }
+}
